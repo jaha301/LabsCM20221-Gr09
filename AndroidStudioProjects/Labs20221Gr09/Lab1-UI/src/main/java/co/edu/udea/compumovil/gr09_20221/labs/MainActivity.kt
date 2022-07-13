aae32c44ef5bc838a00cb9b5a2013cd00ea0cbd4
jaha301 <jaha301@gmail.com>
2022-07-12 20:39:39 -0500

    Lab1 first commit

---
+++ AndroidStudioProjects/Labs20221Gr09/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr09_20221/labs/MainActivity.kt
@@ -0,0 +1,11 @@
+package co.edu.udea.compumovil.gr09_20221.labs
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
